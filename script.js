73064fe3e789550e3b32761758b3b256f2f5f33f
Kevsankyo <159601936+Kevsankyo@users.noreply.github.com>
2024-02-09 21:56:55 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+"use strict";
+
+const titleElement = document.querySelector(".title");
+const buttonsContainer = document.querySelector(".buttons");
+const yesButton = document.querySelector(".btn--yes");
+const noButton = document.querySelector(".btn--no");
+const catImg = document.querySelector(".cat-img");
+const videoElement = document.querySelector('.video');
+
+const MAX_IMAGES = 5;
+
+let play = true;
+let noCount = 0;
+
+yesButton.addEventListener("click", handleYesClick);
+
+noButton.addEventListener("click", function () {
+  if (play) {
+    noCount++;
+    const imageIndex = Math.min(noCount, MAX_IMAGES);
+    changeImage(imageIndex);
+    resizeYesButton();
+    updateNoButtonText();
+    if (noCount === MAX_IMAGES) {
+      play = false;
+    }
+  }
+});
+
+function handleYesClick() {
+  titleElement.innerHTML = "YEII TE ADORO MUCHO ( somos esos d arriba )";
+  buttonsContainer.classList.add("hidden");
+  changeImage("yes");
+  showVideo();
+}
+
+function resizeYesButton() {
+  const computedStyle = window.getComputedStyle(yesButton);
+  const fontSize = parseFloat(computedStyle.getPropertyValue("font-size"));
+  const newFontSize = fontSize * 1.6;
+
+  yesButton.style.fontSize = `${newFontSize}px`;
+}
+
+function generateMessage(noCount) {
+  const messages = [
+    "No",
+    "¿Estás segura?😾",
+    "Pookie, por favor😿",
+    "No hagas me hagas esto :(",
+    "Romperas mi pichurro corazon",
+    "¡Voy a llorar...",
+  ];
+
+  const messageIndex = Math.min(noCount, messages.length - 1);
+  return messages[messageIndex];
+}
+
+function changeImage(image) {
+  catImg.src = `img/cat-${image}.jpg`;
+}
+
+function updateNoButtonText() {
+  noButton.innerHTML = generateMessage(noCount);
+}
+
+function showVideo() {
+  videoElement.classList.remove('hidden');
+}
